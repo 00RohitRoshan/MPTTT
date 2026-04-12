c2db697bab27be28eac41548affddebc3449dce5
rohit roshan jena <rohit.r@iserveu.co.in>
2026-04-12 19:09:54 +0530

    connection established

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Patch for Nakama SDK bug in some browser environments
+if (typeof XMLHttpRequest !== 'undefined' && !Object.getOwnPropertyDescriptor(XMLHttpRequest.prototype, "withCredentials")?.set) {
+  Object.defineProperty(XMLHttpRequest.prototype, "withCredentials", {
+    set: function() {},
+    configurable: true
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
